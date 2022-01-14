67975899191f4ace30e41e90f600df88a37895e5
Yago Batista <yagobatistafis@yahoo.com.br>
2022-01-14 16:19:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import CoinPage from "./pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
+import Alert from "./components/Alert";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    minHeight: "100vh",
+    color: "white",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={HomePage} exact />
+        <Route path="/coins/:id" component={CoinPage} />
+      </div>
+      <Alert />
+    </>
   );
 }
 
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAq0pv0x4F7ZsnhO3T67lbwuiPZ6w9l0o",
+  authDomain: "crypto-tracker-3e62e.firebaseapp.com",
+  projectId: "crypto-tracker-3e62e",
+  storageBucket: "crypto-tracker-3e62e.appspot.com",
+  messagingSenderId: "948941384932",
+  appId: "1:948941384932:web:af2122b84f33f1101790f1",
+  measurementId: "G-VFS81TBM8J",
+};
+
+export default firebaseConfig;
